3f0edceecff559c171b29dbb6f333d45fe9e278b
Hissen Tutu <hissentutu@gmail.comm>
2026-01-08 10:44:31 +0300

    stopped the ENV

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 /* jobs.start(); */
-if (process.env.NODE_ENV === "production") jobs.start();
+/* if (process.env.NODE_ENV === "production") */ jobs.start();
 
 app.use(
   cors({
