82502ba80bcee2df3748f6b80288718a3f234082
Hissen Tutu <hissentutu@gmail.comm>
2026-01-08 10:57:13 +0300

    adjusted the Get in cron

--- src/lib/cron.js
+++ src/lib/cron.js
@@ -4,9 +4,9 @@ import https from "https";
 const jobs = new cron.CronJob("*/49 * * * * * ", function () {
   https
     .get(process.env.API_URL, (res) => {
-      if (res.statusCode === 200)
+      if (res.statusCode === 200) {
         console.log("GET running on every 14 minutes");
-      else console.error("get request failed", res.statusCode);
+      } else console.error("get request failed", res.statusCode);
     })
     .on("error", (e) => console.log(`res response failed ${e}`));
 });
